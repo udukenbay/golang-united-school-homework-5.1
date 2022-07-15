c4846dc57ddcef17deeaf164e51aedc1391bfb19
Дукенбай Урзия Әбдіхалыққызы <Urziya.Dukenbay@kaspi.kz>
2022-07-15 16:11:12 +0600

    square struct

--- square.go
+++ square.go
@@ -1,5 +1,27 @@
 package square
 
+//package main
+
+import "fmt"
+
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Printf("area     : %f \n", g.Area())
+	fmt.Printf("perimeter: %f \n", g.Perimeter())
+}
+
+func main() {
+	p := Point{3, 5}
+	s := Square{a: 4}
+	fmt.Printf("%.3f\n", s.End(p))
+	measure(&s)
+}
+
+type geometry interface {
+	Area() uint
+	Perimeter() uint
+}
+
 type Point struct {
 	x, y int
 }
@@ -9,8 +31,7 @@ type Square struct {
 	a     uint
 }
 
-func (sq *Square) End() Point {
-	var p Point = Point{x: 1, y: 2}
+func (sq Square) End(p Point) Point {
 	p.x = p.x + int(sq.a)
 	p.y = p.y - int(sq.a)
 	return p
@@ -20,6 +41,6 @@ func (sq *Square) Area() uint {
 	return sq.a * sq.a
 }
 
-func (sq *Square) Perimeter() uint {
+func (sq Square) Perimeter() uint {
 	return 4 * sq.a
 }
